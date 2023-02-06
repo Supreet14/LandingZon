2ec51e870ca165e26f9bda95731154448ae8f5e1
Supreet14 <55839742+Supreet14@users.noreply.github.com>
2023-02-06 18:15:03 +0530

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "landing-zone" {
-  name          = "landing-zone-su14"
+  name          = "landing-zone-su142"
   force_destroy = false
   location      = "US"
   storage_class = "STANDARD"
