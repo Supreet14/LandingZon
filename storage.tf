4539f855e9715366c9242ea83ae4bb7b684fbea9
Supreet14 <55839742+Supreet14@users.noreply.github.com>
2023-02-07 07:13:34 +0530

    Update and rename storage.tf to backend.tf

--- storage.tf
+++
@@ -1,9 +0,0 @@
-resource "google_storage_bucket" "landing-zone" {
-  name          = "landing-zone-su142"
-  force_destroy = false
-  location      = "US"
-  storage_class = "STANDARD"
-  versioning {
-    enabled = true
-  }
-}
